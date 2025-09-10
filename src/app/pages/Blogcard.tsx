133221783881303b07c41cba2d81388f0394968c
Amankhare01 <amankhare.aa@gmail.com>
2025-09-10 17:58:02 +0530

    Add new images and update blog image paths for consistency

--- src/app/pages/Blogcard.tsx
+++ src/app/pages/Blogcard.tsx
@@ -24,11 +24,11 @@ export default function BlogCard({ title, image, date, url, description }: BlogC
         <p className="flex items-center text-sm text-blue-600 mb-2">
           <FaRegClock className="mr-2" /> {date}
         </p>
-        <h3 className="font-semibold text-lg mb-2">{title}</h3>
+        <h3 className="font-semibold text-gray-600 text-lg mb-2">{title}</h3>
         <p className="text-gray-600 text-sm mb-4 line-clamp-3">{description}</p>
         <Link
           href={url}
-          className="inline-block border border-gray-800 px-4 py-2 rounded text-sm hover:bg-gray-800 hover:text-white transition"
+          className="inline-block border border-gray-800  px-4 py-2 rounded text-gray-600 text-sm hover:bg-gray-800 hover:text-white transition"
         >
           Read More
         </Link>
--- src/app/pages/blogdata.js
+++ src/app/pages/blogdata.js
@@ -1,7 +1,7 @@
 export const blogs = [
   {
     title: "How to Fix Slow Internet and Connectivity Issues",
-    image: "/assets/1.jpg",
+    image: "/images/1.jpg",
     date: "February 10, 2025",
     url: "/blogs/Slow-Internet-Issues",
     description:
@@ -9,7 +9,7 @@ export const blogs = [
   },
   {
     title: "Common Computer Problems & How to Fix Them",
-    image: "/assets/2.jpg",
+    image: "/images/2.jpg",
     date: "February 14, 2025",
     url: "/Blogs/Computer-Problem",
     description:
@@ -17,7 +17,7 @@ export const blogs = [
   },
   {
     title: "Protecting Your Devices from Viruses & Malware",
-    image: "/assets/3.jpg",
+    image: "/images/3.jpg",
     date: "February 18, 2025",
     url: "/Blogs/Protecting-Your-Devices-from-Viruses-&-Malware",
     description:
@@ -25,7 +25,7 @@ export const blogs = [
   },
   {
     title: "Remote Tech Support: How It Works & When You Need It",
-    image: "/assets/4.jpg",
+    image: "/images/4.jpg",
     date: "February 22, 2025",
     url: "/Blogs/Remote-Tech-Support",
     description:
@@ -33,7 +33,7 @@ export const blogs = [
   },
   {
     title: "Best Tips to Optimize Your PC Performance",
-    image: "/assets/5.jpg",
+    image: "/images/5.jpg",
     date: "February 26, 2025",
     url: "/Blogs/Best-Tips-to-Optimize-Your-PC-Performance",
     description:
@@ -41,7 +41,7 @@ export const blogs = [
   },
   {
     title: "How AI-Powered Chatbots Are Revolutionizing Tech Support in 2025",
-    image: "/assets/6.jpg",
+    image: "/images/6.jpg",
     date: "April 2, 2025",
     url: "/Blogs/AI-Powered",
     description:
@@ -49,7 +49,7 @@ export const blogs = [
   },
   {
     title: "Millennials vs. Gen Z: How Their Customer Support Expectations Differ",
-    image: "/assets/7.jpg",
+    image: "/images/7.jpg",
     date: "April 8, 2025",
     url: "/blogs/Tech-support/",
     description:
@@ -57,7 +57,7 @@ export const blogs = [
   },
   {
     title: "The Most Frequent Questions We Get at Our Help Desk",
-    image: "/assets/8.jpg",
+    image: "/images/8.jpg",
     date: "April 14, 2025",
     url: "/blogs/help-desk-questions/",
     description:
@@ -65,7 +65,7 @@ export const blogs = [
   },
   {
     title: "How Artificial Intelligence Can Improve IT for Businesses",
-    image: "/assets/9.jpg",
+    image: "/images/9.jpg",
     date: "May 6, 2025",
     url: "/blogs/ai-in-it-support/",
     description:
@@ -73,7 +73,7 @@ export const blogs = [
   },
   {
     title: "Standard Operating Procedures (SOPs) and Training",
-    image: "/assets/sops.png",
+    image: "/images/sops.png",
     date: "May 12, 2025",
     url: "/blogs/Standard_Operating_Procedures",
     description:
@@ -81,7 +81,7 @@ export const blogs = [
   },
   {
     title: "How Remote Support Works: Behind the Scenes of Fixing Tech Issues",
-    image: "/assets/remote-support.jpg",
+    image: "/images/remote-support.jpg",
     date: "June 9, 2025",
     url: "/blogs/How_Remote_Support_Works",
     description:
@@ -89,7 +89,7 @@ export const blogs = [
   },
   {
     title: "How to Secure Your System During Remote Support Sessions",
-    image: "/assets/secure.jpg",
+    image: "/images/secure.jpg",
     date: "June 18, 2025",
     url: "/blogs/Secure_Remote_Support_Sessions",
     description:
